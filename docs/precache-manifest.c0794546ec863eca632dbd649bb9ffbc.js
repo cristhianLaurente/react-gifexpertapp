3a1c3c8e11958425e69f52e9dd26c12f1d22fb31
cristhianLaurente <claurente.1999@gmail.com>
2020-09-01 06:23:36 -0500

    docs add

---
+++ docs/precache-manifest.c0794546ec863eca632dbd649bb9ffbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c34ce184a4676343051000fa6e5aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30385441b906f5f91ae6",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "05a07b4f7de8f2d942d7",
+    "url": "/static/css/main.57af0c90.chunk.css"
+  },
+  {
+    "revision": "30385441b906f5f91ae6",
+    "url": "/static/js/2.9fff9853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fff9853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a07b4f7de8f2d942d7",
+    "url": "/static/js/main.d67dace0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
